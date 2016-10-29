c9f2ffe9624b421645a449d651a9c4f9e43562e6
Denis Michailov <kamrad.reg@gmail.com>
2016-10-29 22:58:18 +1000

    rebase to mongo starts

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,62 +1,54 @@
 var express = require('express');
 var bookRouter = express.Router();
-var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: 'db4free.net',
-	// port: 3304,
-	user: 'kamrade',
-	password: 'kamrade',
-	database: 'kamrade'
-});
-
-connection.connect(function(err) {
-	if (err) {
-		console.error('connection error: ' + err.stack);
-		return;
-	} else {
-		console.log('connected as ' + connection.threadId);
+var books = [
+	{
+		title: 'War and Peace',
+		genre: 'Historical Fiction',
+		author: 'Lev Nikolaevich Tolstoy',
+		read: false
+	},
+	{
+		title: 'The Time Machine',
+		genre: 'Science Fiction',
+		author: 'H. G. Wells',
+		read: false
+	},
+	{
+		title: 'Les Miserables',
+		genre: 'Historical Fiction',
+		author: 'Victor Hugo',
+		read: true
 	}
-});
+];
 
 var router = function(nav) {
 
-	var books = [];
 	bookRouter.route('/')
 		.get(function(req, res) {
-			connection.query('select * from books', function(err, rows) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				books = rows;
-				res.render('bookListView', {
-					title: 'Bookz',
-					nav: nav,
-					books: books
-				});
+			res.render('bookListView', {
+				title: 'Bookz',
+				nav: nav,
+				books: books
 			});
 		});
 
 	bookRouter.route('/:id')
 		.all(function(req, res, next) {
 			var id = req.params.id;
-			var sqlQuery = 'select * from books where id = ' + connection.escape(id);
-			connection.query(sqlQuery, function(err, recordset) {
-				if (recordset.length === 0) {
-					res.status(404).send('Not Found');
-					return;
-				} else {
-					req.book = recordset[0];
-					next();
-				}
-			});
+			if ((books.length - 1) < id) {
+				res.status(404).send('Not Found');
+				return;
+			} else {
+				next();
+			}
 		})
 		.get(function(req, res) {
+			var id = req.params.id;
 			res.render('bookView', {
 				title: 'Books',
 				nav: nav,
-				book: req.book
+				book: books[id]
 			});
 		});
 
